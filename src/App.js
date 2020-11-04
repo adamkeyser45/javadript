5d6a8549de33484cf02dce99560052e939866943
Adam Keyser <65785770+adamkeyser45@users.noreply.github.com>
2020-11-03 23:36:12 -0600

    redo the files to make sure templates work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+
+import Pricing from './components/pages/Pricing';
 import SignInSide from './components/pages/SignInSide';
+import SignUp from './components/pages/SignUp';
+import About from './components/pages/About';
 
 function App() {
   return (
     <div>
-      <SignInSide></SignInSide>
+      
+      {/* <Pricing></Pricing> */}
+      <About></About>
+      {/* <SignInSide></SignInSide> */}
+      {/* <SignUp></SignUp> */}
+
+      
     </div>
   );
 }
