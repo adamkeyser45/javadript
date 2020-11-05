729fb589770f1a616bfb59b9f9fd01c693310b75
jtillson1 <65985044+jtillson1@users.noreply.github.com>
2020-11-04 21:00:29 -0600

    assets/img

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './assets/style.css'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
