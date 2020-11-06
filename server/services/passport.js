7bc88179ce8aee71975206b1ab84334efb7f65db
Vox Humana <jonprine@gmail.com>
2020-11-06 13:36:11 -0600

    updated passport.js

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,31 +1,30 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const mongoose = require("mongoose");
-
-
 const User = mongoose.model("users");
 
 passport.serializeUser((user, done) => {
-    done(null, user.id);
+  done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
-    User.findById(id).then(user => {
-        done(null, user);
-    });
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
 });
 
 passport.use(
   new GoogleStrategy(
     {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback",
+      clientID: process.env.googleClientID,
+      clientSecret: process.env.googleClientSecret,
+      callbackURL: "http://localhost:3001/auth/google/callback",
+      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
     },
     (accessToken, refreshToken, profile, done) => {
-        console.log('access token', accessToken);
-        console.log('refresh token', refreshToken);
-        console.log('profile:', profile);
+      console.log("access token", accessToken);
+      console.log("refresh token", refreshToken);
+      console.log("profile:", profile);
       User.findOne({ googleId: profile.id }).then((existingUser) => {
         if (existingUser) {
           // we already have a record with the given profile ID
@@ -34,7 +33,7 @@ passport.use(
           // we don't have a user record with this ID, make a new record
           new User({ googleId: profile.id })
             .save()
-            .then(user => done(null, user));
+            .then((user) => done(null, user));
         }
       });
     }
