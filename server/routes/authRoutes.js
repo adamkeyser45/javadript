282793ff354e89e53b7111a8d9a28c6d58ca8507
Vox Humana <jonprine@gmail.com>
2020-11-07 11:42:58 -0600

    installed passport facebook set up middleware

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,23 +1,39 @@
-const passport = require('passport');
+const passport = require("passport");
 
-module.exports = app => {
+module.exports = (app) => {
   app.get(
-    '/auth/google',
-    passport.authenticate('google', {
-      scope: ['profile', 'email']
+    "/auth/google",
+    passport.authenticate("google", {
+      scope: ["profile", "email"],
     })
   );
 
-  app.get('/auth/google/callback', passport.authenticate('google'), (req, res) => {
-    res.redirect("/");
+  app.get(
+    "/auth/google/callback",
+    passport.authenticate("google"),
+    (req, res) => {
+      res.redirect("/");
+    }
+  );
+
+  app.get("/api/logout", (req, res) => {
+    req.logout();
+    res.send(req.user);
   });
 
-  app.get('/api/logout', (req, res) => {
-      req.logout();
-      res.send(req.user);
+  app.get("/api/current_user", (req, res) => {
+    res.send(req.user);
   });
 
-  app.get('/api/current_user', (req, res) => {
-      res.send(req.user);
-  });
+  // Facebook OAuth middleware
+  app.get("/auth/facebook", passport.authenticate("facebook"));
+
+  app.get(
+    "/auth/facebook/callback",
+    passport.authenticate("facebook", { failureRedirect: "/login" }),
+    function (req, res) {
+      // Successful authentication, redirect home.
+      res.redirect("/");
+    }
+  );
 };
