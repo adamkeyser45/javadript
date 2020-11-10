6602efe37c148d88bb1cd7eea4961689f49e4efb
Vox Humana <jonprine@gmail.com>
2020-11-09 20:06:50 -0600

    update to typedefs

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,37 @@
+const { AuthenticationError } = require("apollo-server-express");
+const { User } = require("../models");
+
 const resolvers = {
-    Query: {
-      helloWorld: () => {
-        return 'Hello world!';
+  Query: {
+    helloWorld: () => {
+      return "Hello world!";
+    },
+    user: async (parent, args, context) => {
+      if (context.user) {
+        const user = await await User.findById(context.user._id);
+        return user;
       }
-    }
-  };
-  module.exports = resolvers;
+    },
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+      const token = signToken(user);
+      return { token, user };
+    },
+  },
+};
+module.exports = resolvers;
--- server/services/passport.js
+++ server/services/passport.js
@@ -17,59 +17,59 @@ passport.deserializeUser((id, done) => {
 });
 
 // Google strategy
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: "http://localhost:3001/auth/google/callback",
-      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log("access token", accessToken);
-      console.log("refresh token", refreshToken);
-      console.log("profile:", profile);
-      User.findOne({ googleId: profile.id }).then((existingUser) => {
-        if (existingUser) {
-          // we already have a record with the given profile ID
-          done(null, existingUser);
-        } else {
-          // we don't have a user record with this ID, make a new record
-          new User({ googleId: profile.id })
-            .save()
-            .then((user) => done(null, user));
-        }
-      });
-    }
-  )
-);
+// passport.use(
+//   new GoogleStrategy(
+//     {
+//       clientID: keys.googleClientID,
+//       clientSecret: keys.googleClientSecret,
+//       callbackURL: "http://localhost:3001/auth/google/callback",
+//       userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+//     },
+//     (accessToken, refreshToken, profile, done) => {
+//       console.log("access token", accessToken);
+//       console.log("refresh token", refreshToken);
+//       console.log("profile:", profile);
+//       User.findOne({ googleId: profile.id }).then((existingUser) => {
+//         if (existingUser) {
+//           // we already have a record with the given profile ID
+//           done(null, existingUser);
+//         } else {
+//           // we don't have a user record with this ID, make a new record
+//           new User({ googleId: profile.id })
+//             .save()
+//             .then((user) => done(null, user));
+//         }
+//       });
+//     }
+//   )
+// );
 
 // Facebook strategy
-passport.use(
-  new FacebookStrategy(
-    {
-      clientID: keys.facebookClientId,
-      clientSecret: keys.facebookClientSecret,
-      callbackURL: "http://localhost:3001/auth/facebook/callback"
-    },
-    (accessToken, refreshToken, profile, done) => {
-      console.log("access token", accessToken);
-      console.log("refresh token", refreshToken);
-      console.log("profile:", profile);
-      User.findOne({ facebookId: profile.id }).then((existingUser) => {
-        if (existingUser) {
-          // we already have a record with the given profile ID
-          done(null, existingUser);
-        } else {
-          // we don't have a user record with this ID, make a new record
-          new User({ facebookId: profile.id })
-            .save()
-            .then((user) => done(null, user));
-        }
-      });
-    }
-  )
-);
+// passport.use(
+//   new FacebookStrategy(
+//     {
+//       clientID: keys.facebookClientId,
+//       clientSecret: keys.facebookClientSecret,
+//       callbackURL: "http://localhost:3001/auth/facebook/callback"
+//     },
+//     (accessToken, refreshToken, profile, done) => {
+//       console.log("access token", accessToken);
+//       console.log("refresh token", refreshToken);
+//       console.log("profile:", profile);
+//       User.findOne({ facebookId: profile.id }).then((existingUser) => {
+//         if (existingUser) {
+//           // we already have a record with the given profile ID
+//           done(null, existingUser);
+//         } else {
+//           // we don't have a user record with this ID, make a new record
+//           new User({ facebookId: profile.id })
+//             .save()
+//             .then((user) => done(null, user));
+//         }
+//       });
+//     }
+//   )
+// );
 
 // Github strategy
 passport.use(
