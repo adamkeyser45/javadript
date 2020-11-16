7be67e26e9b986fc0766deaff44540cd9dc59e0c
Adam Keyser <65785770+adamkeyser45@users.noreply.github.com>
2020-11-15 20:56:41 -0600

    undoing what I did for last commit

--- server/server.js
+++ server/server.js
@@ -44,7 +44,7 @@ app.get("*", (req, res) => {
 });
 
 db.once("open", () => {
-  app.listen(process.env.PORT, () => {
+  app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
